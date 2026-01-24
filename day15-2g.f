32eae37da5362dba4fe7b6882c843e4721e3b88b
Tim Holyoake <timholyoake@btopenworld.com>
2026-01-24 09:33:43 +0000

    Day15-2g.f cleaned up, now also implements part 1

--- day15-2g.f
+++ day15-2g.f
@@ -1,76 +1,67 @@
       PROGRAM DAY15P2
 C
       CHARACTER*40 LINE 
-      INTEGER LOOKUPLOW(0:30000000),LASTSAID
-C     LOOKUPLOW(X)= last time X said (for X=0 to 30,000,000)
-      INTEGER LNUML,LNUM,NUM,L1
+      INTEGER LOOKUP(0:30000000),LASTSAID
+C     LOOKUP(X)= last time X said (for X=0 to 30,000,000)
+      INTEGER LNUM,NUM,L1
 C
    10 FORMAT(A)
    20 FORMAT(A)
    30 FORMAT(I2)
-   35 FORMAT(I9,A,I9,A,I18)
-   40 FORMAT(A,I18)
+   40 FORMAT(A,I9)
 C
-      WRITE(*,10)"Advent of Code 2020 day 15, part 2"
+      WRITE(*,10)"Advent of Code 2020 day 15, parts 1 and 2"
       WRITE(*,10)" "
 C
       OPEN(10,FILE="day15in.txt",STATUS="OLD",FORM="FORMATTED",
      +     ACCESS="SEQUENTIAL",ACTION="READ")
       READ(10,FMT=20) LINE
       CLOSE(10)
+C     Initialise the LOOKUP array elements to -1
       DO L1=1,30000000
-        LOOKUPLOW(L1)=-1
+        LOOKUP(L1)=-1
       ENDDO
+C     Get the starting numbers
       NUM=1
       DO WHILE (INDEX(LINE,',').NE.0)
-        READ(LINE(1:INDEX(LINE,',')-1),30) LNUML
-        LOOKUPLOW(LNUML)=NUM
+        READ(LINE(1:INDEX(LINE,',')-1),30) LNUM
+        LOOKUP(LNUM)=NUM
         LINE=LINE(INDEX(LINE,',')+1:)
         NUM=NUM+1
       ENDDO
-      READ(LINE,30) LNUML
-      LOOKUPLOW(LNUML)=NUM
-C
-C     Note that the input numbers in the samples and puzzle are all
-C     unique - so these are the first n said numbers. LNUM is the number
-C     of entries used in the LOOKUP array, LNUML is used for low numbers
-C     kept in LOOKUPLOW. All starting numbers go into LOOKUPLOW.
-C
-      LASTSAID=LNUML
+      READ(LINE,30) LNUM
+      LOOKUP(LNUM)=NUM
+C     Note that the starting numbers in the samples and puzzle are all
+C     unique - so these are the first n said numbers.
+      LASTSAID=LNUM
       LNUM=0
-      DO L1=NUM+1,30000000
-        CALL SAYNUM(LOOKUPLOW,LOOKUP,LNUM,L1,LASTSAID)
+      DO L1=NUM+1,2020
+        CALL SAYNUM(LOOKUP,L1,LASTSAID)
       ENDDO
-      WRITE(*,40)"The 30,000,000th number to be spoken is "
-     +           ,LASTSAID
+      WRITE(*,40)"The 2,020th number to be spoken is ",LASTSAID
+      DO L1=2021,30000000
+        CALL SAYNUM(LOOKUP,L1,LASTSAID)
+      ENDDO
+      WRITE(*,40)"The 30,000,000th number to be spoken is ",LASTSAID
       END
 C
-      SUBROUTINE SAYNUM(LOOKUPLOW,LOOKUP,LNUM,NTHNUM,LASTSAID)
-      INTEGER SAY,LASTSAID,LOOKUPLOW(0:30000000)
-      INTEGER LNUM,NTHNUM,L1
+      SUBROUTINE SAYNUM(LOOKUP,NTHNUM,LASTSAID)
+      INTEGER LOOKUP(0:30000000),NTHNUM,LASTSAID
+      INTEGER SAY
 C
-      IF (LASTSAID.LE.30000000) THEN
-        IF (LOOKUPLOW(LASTSAID).NE.-1) THEN
-C           This number has been said before
-            SAY=(NTHNUM-1)-LOOKUPLOW(LASTSAID)
-            LOOKUPLOW(LASTSAID)=NTHNUM-1
-C           Break out of the loop
-            GOTO 100
-        ENDIF
+C     There's a problem - the number is bigger than we can store!
+      IF (LASTSAID.GT.30000000) STOP 8
+C
+      IF (LOOKUP(LASTSAID).NE.-1) THEN
+C       This number has been said before so can calculate next to SAY
+        SAY=(NTHNUM-1)-LOOKUP(LASTSAID)
       ELSE
-C       There's a problem - the number is bigger than we can store!
-        STOP 8
+C       LASTSAID is new
+        SAY=0
       ENDIF
-C     Can only get here if LASTSAID is new
-      IF (LASTSAID.LT.30000000) THEN
-        LOOKUPLOW(LASTSAID)=NTHNUM-1
-      ELSE
-C       There's a problem - the number is bigger than we can store!
-        STOP 8
-      ENDIF
-      SAY=0
-  100 CONTINUE
+C     Update the last time we said LASTSAID ...
+      LOOKUP(LASTSAID)=NTHNUM-1
+C     ... and update LASTSAID
       LASTSAID=SAY
-C
       RETURN
       END
